1ef7df0623e287338087acb21f452944af5a2b9f
unknown <csl4wd@virginia.edu>
2020-10-04 18:23:55 -0400

    updated bootstrap.sh again, first actual draft

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,5 @@
 /usr/bin/yum install -y python-pip httpd unzip
 /bin/pip3 install awscli
 /bin/aws s3api get-object --bucket cs4740-csl4wd-private --key bundle.zip bundle.zip
+/bin/unzip bundle.zip -d /etc/httpd/conf/httpd.conf
+/etc/init.d/httpd start
