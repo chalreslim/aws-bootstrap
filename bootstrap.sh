b5a4b1eb9a965848c7a6c7d529aa893308c82ddc
unknown <csl4wd@virginia.edu>
2020-10-04 18:54:45 -0400

    added sudo to a couple commands, pip3->pip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/usr/bin/yum update -y
-/bin/amazon-linux-extras install -y epel
+sudo /usr/bin/yum update -y
+sudo /bin/amazon-linux-extras install -y epel
 /usr/bin/yum install -y python-pip httpd unzip
-/bin/pip3 install awscli
+sudo /bin/pip install awscli
 /bin/aws s3api get-object --bucket cs4740-csl4wd-private --key bundle.zip bundle.zip
 /bin/unzip bundle.zip -d /etc/httpd/conf/httpd.conf
 /etc/init.d/httpd start
