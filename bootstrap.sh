428efac70a667ceab5a126d3c2868afc0db9043b
unknown <csl4wd@virginia.edu>
2020-10-04 18:17:27 -0400

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,6 @@
 
 /usr/bin/yum update -y
 /bin/amazon-linux-extras install -y epel
-/usr/bin/yum install -y git python3 python3-dev python3-pip nfs-utils
-/bin/pip3 install boto3 pandas requests
+/usr/bin/yum install -y python-pip httpd unzip
+/bin/pip3 install awscli
+/bin/aws s3api get-object --bucket cs4740-csl4wd-private --key bundle.zip bundle.zip
