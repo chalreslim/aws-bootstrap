af6beb31fa3c42a0da2f1a6b06a9a95106fc024f
unknown <csl4wd@virginia.edu>
2020-10-04 20:07:35 -0400

    updated to be actually correct

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-sudo /usr/bin/yum update -y
-sudo /bin/amazon-linux-extras install -y epel
-/usr/bin/yum install -y python-pip httpd unzip
-sudo /bin/pip install awscli
+/bin/yum update -y
+/bin/yum install -y python-pip httpd unzip
+/bin/pip install awscli
 /bin/aws s3api get-object --bucket cs4740-csl4wd-private --key bundle.zip bundle.zip
-/bin/unzip bundle.zip -d /etc/httpd/conf/httpd.conf
-/etc/init.d/httpd start
-/sbin/reboot
+/bin/unzip bundle.zip -d /var/www/html
+systemctl enable httpd
+system start httpd
